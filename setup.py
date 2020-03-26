3f1b5381ffad5c4a39c06a5ec37e68c404d1e351
JEJ <ejjun@hansol.com>
2020-03-26 17:04:37 +0900

    install error 수정

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
-from pip.req import parse_requirements
+#from pip.req import parse_requirements
 from setuptools import setup, find_packages
+
 
 def install():
     required = []
@@ -18,23 +19,24 @@ def install():
         author='SuHun Han',
         author_email='ssut@ssut.me',
         url='https://github.com/ssut/py-hanspell',
-        classifiers = ['Development Status :: 5 - Production/Stable',
-            'Intended Audience :: Education',
-            'Intended Audience :: End Users/Desktop',
-            'License :: Freeware',
-            'Operating System :: POSIX',
-            'Operating System :: Microsoft :: Windows',
-            'Operating System :: MacOS :: MacOS X',
-            'Topic :: Education',
-            'Programming Language :: Python',
-            'Programming Language :: Python :: 2.6',
-            'Programming Language :: Python :: 2.7',
-            'Programming Language :: Python :: 3.2',
-            'Programming Language :: Python :: 3.3',
-            'Programming Language :: Python :: 3.4'],
+        classifiers=['Development Status :: 5 - Production/Stable',
+                     'Intended Audience :: Education',
+                     'Intended Audience :: End Users/Desktop',
+                     'License :: Freeware',
+                     'Operating System :: POSIX',
+                     'Operating System :: Microsoft :: Windows',
+                     'Operating System :: MacOS :: MacOS X',
+                     'Topic :: Education',
+                     'Programming Language :: Python',
+                     'Programming Language :: Python :: 2.6',
+                     'Programming Language :: Python :: 2.7',
+                     'Programming Language :: Python :: 3.2',
+                     'Programming Language :: Python :: 3.3',
+                     'Programming Language :: Python :: 3.4'],
         packages=find_packages(),
         install_requires=required,
     )
+
 
 if __name__ == "__main__":
     install()
